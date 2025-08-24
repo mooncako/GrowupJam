d4d7068e062cb7bb8623baf4f9c71c32ed3e3393
Pat Wang <mooncako@gmail.com>
2025-08-24 14:56:20 -0700

    Added a burst vfx

--- Assets/Scripts/SunlightOrb.cs
+++ Assets/Scripts/SunlightOrb.cs
@@ -1,17 +1,23 @@
 using Sirenix.OdinInspector;
 using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class SunlightOrb : NetworkBehaviour
 {
     [SerializeField, BoxGroup("Settings")] private LayerMask _playerMask;
+    [SerializeField, BoxGroup("References")] private GameObject _orb;
+    [SerializeField, BoxGroup("References")] private VisualEffect _burst;
 
     void OnTriggerEnter(Collider other)
     {
         if ((_playerMask.value & (1 << other.gameObject.layer)) != 0)
         {
             other.GetComponent<PlayerController>().OnReceiveSunlight();
-            Destroy(gameObject);
+            _orb.SetActive(false);
+            _burst.Play();
+
+            Destroy(gameObject, .5f);
         }
     }
 }
